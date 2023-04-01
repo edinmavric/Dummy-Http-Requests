c0112376717847c6bf3f6c31479c5bbfc87bc45e
Edin Mavric <edinmavric10@gmail.com>
2023-04-01 15:34:42 +0200

    getting data with fetch() api

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,31 +5,29 @@ const fetchButton = document.querySelector("#available-posts button");
 const postList = document.querySelector("ul");
 
 function sendHttpRequest(method, url, data) {
-  const promise = new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
+  // const promise = new Promise((resolve, reject) => {
+  // const xhr = new XMLHttpRequest();
+  // xhr.open(method, url);
+  // xhr.responseType = "json";
+  // xhr.onload = function () {
+  //   if (xhr.status >= 200 && xhr.status < 300) {
+  //     resolve(xhr.response);
+  //   } else {
+  //     reject(new Error("Something went wrong!"));
+  //   }
+  //   resolve(xhr.response);
+  //   const listOfPosts = JSON.parse(xhr.response);
+  // };
+  // xhr.onerror = function () {
+  //   reject(new Error("Failed to send request!"));
+  // };
+  // xhr.send(JSON.stringify(data));
+  // });
 
-    xhr.open(method, url);
-
-    xhr.responseType = "json";
-
-    xhr.onload = function () {
-      if (xhr.status >= 200 && xhr.status < 300) {
-        resolve(xhr.response);
-      } else {
-        reject(new Error("Something went wrong!"));
-      }
-      resolve(xhr.response);
-      //   const listOfPosts = JSON.parse(xhr.response);
-    };
-
-    xhr.onerror = function () {
-      reject(new Error("Failed to send request!"));
-    };
-
-    xhr.send(JSON.stringify(data));
+  // return promise;
+  return fetch(url).then((response) => {
+    return response.json();
   });
-
-  return promise;
 }
 
 async function fetchPosts() {
